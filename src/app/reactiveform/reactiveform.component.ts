ca18f9a4d4f554b412ddbe5f23d3f47b4fd3c7b6
unknown <ianprdhn@gmail.com>
2022-01-19 09:22:57 +0545

    first-commit

--- src/app/reactiveform/reactiveform.component.ts
+++ src/app/reactiveform/reactiveform.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Country } from '../model/country';
 
 @Component({
   selector: 'app-reactiveform',
@@ -8,19 +9,36 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 })
 export class ReactiveformComponent implements OnInit {
 
+  countries: Country[] = [
+    new Country(1, 'Nepal'),
+    new Country(2, 'USA'),
+    new Country(3, 'Canada'),
+    new Country(4, 'Australia')
+  ];
+
   constructor(private fb : FormBuilder) { }
 
   ngOnInit(): void {
   }
 
   loginForm: FormGroup = this.fb.group({
-    firstname: [''],
-    lastname : [''],
-    email : ['']
+    name: ['',[ Validators.required, Validators.minLength(3)] ],
+    email : ['', [ Validators.required, Validators.email]],
+    number: ['',  
+    [
+      Validators.required, 
+      Validators.minLength(10), 
+      Validators.maxLength(10)
+    ]
+  ],
+    countryId: ['', [Validators.required]],
+    message : ['', [Validators.required]]
   });
 
   onSubmit() {
     console.log(this.loginForm.value);
   }
-
+  get Control(){
+    return this.loginForm.controls;
+  }
 }
